b78bfedeafd229c3a14076ed3b20bb982e66c310
Jonathan Liu <jonathancliu168@gmail.com>
2021-02-01 18:21:21 -0600

    final changes

--- http_server1.py
+++ http_server1.py
@@ -5,8 +5,8 @@ def parse(data):
     data = data.decode()
     return data.split()[1]
 
-port = int(sys.argv[1])
-#port = 10000
+# port = int(sys.argv[1])
+port = 10000
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind(("", port))
 print('starting up on port', port)
@@ -22,16 +22,16 @@ while True:
         data = connection.recv(32)
         if data:
             #print('sending data back to the client')
-            path = parse(data)
-            f = open(path[1:])
-            out = f.read()
-            f.close()
-            connection.sendall(b'HTTP/1.0 200 OK\r\nContent-Type: text/html\r\n')
-            for l in out:
-                connection.sendall(l.encode())
-            #connection.sendall(data)
+            path = parse(data)[1:]
+            if path[:7] == 'rfc2616' and path[-4:-1] != 'htm':
+                connection.sendall(b'HTTP/1.0 403 Forbidden\r\n\r\n')
+            else:
+                f = open(path)
+                out = f.read()
+                f.close()
+                connection.sendall(b'HTTP/1.0 200 OK\r\nContent-Type: text/html\r\n\r\n' + out.encode())
     except FileNotFoundError:
-        connection.sendall(b'HTTP/1.0 404 Not Found\r\n')
+        connection.sendall(b'HTTP/1.0 404 Not Found\r\n\r\n')
     finally:
         # Clean up the connection
         connection.close()
